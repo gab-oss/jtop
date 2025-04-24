2bd5190efc35d5b91a15ac560e1f2bf893c69e50
Gabriela Ossowska <gab.ossowska@gmail.com>
2025-04-24 16:10:00 +0200

    init

---
+++ src/main/java/org/dolniak/jtop/JtopApplication.java
@@ -0,0 +1,13 @@
+package org.dolniak.jtop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dolniak/jtop/JtopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dolniak.jtop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
