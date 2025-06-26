b239dc7086faf37be97f5fe8df8cd537b1d72f26
Gabriela Ossowska <gab.ossowska@gmail.com>
2025-06-27 00:39:17 +0200

    add minimal js to show the processes list

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,1 +1,34 @@
+async function getData() {
+    const url = "/processes";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const json = await response.json();
+      console.log(json);
+      return json;
+    } catch (error) {
+      console.error(error.message);
+    }
+}
+
+async function fillTable() {
+  let items = await getData();
+
+  const table = document.getElementById("testBody");
+  items.forEach( item => {
+    let row = table.insertRow();
+    let date = row.insertCell(0);
+    date.innerHTML = item.pid;
+    let name = row.insertCell(1);
+    name.innerHTML = item.command;
+  });
+}
+
+window.addEventListener("DOMContentLoaded", fillTable);
+
+
+
 
